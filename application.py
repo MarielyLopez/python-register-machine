fdf438a37d1a91c95f036bf22ad52be19cacab4f
MarielyLopez <mariielylopez795@gmail.com>
2015-05-20 15:27:55 -0600

    Stories 1 and 2 made, the program displays the menu and allows the user to enter an option

--- application.py
+++ application.py
@@ -1,3 +1,24 @@
-# Aqui escribe tu codigo
+"""Register Machine"""
 
-print "Hello World"
+def menu():
+    print " "
+    print "    Menu"
+    print " "
+    print "  1. Add an item: "
+    print "  2. Sell Articles: "
+    print "  3. Exit. "
+    while True:
+        answer = raw_input("Enter the number of your choice: ")
+        if answer == "1":
+            print "Add an item"
+            break #Esto es para que me corte el programa cuando la funcion es true"
+        elif answer == "2":
+            print "Sell Articles"
+            break
+        elif answer == "3":
+            print "Exit"
+            break
+        else:
+            print "Insert a number of 1 of 3"
+
+menu()
