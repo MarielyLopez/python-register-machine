51b8693c3fddeec660bb0788a251a6b2c7cf9003
MarielyLopez <mariielylopez795@gmail.com>
2015-05-21 14:26:12 -0600

    Prueba

--- application.py
+++ application.py
@@ -4,16 +4,21 @@ def answer_y_n():
             add_answer = add_answer.lower()
             add_price = raw_input("Price: ")
             print "Do you want to insert another article?"
-            while True:
-                add_answer1 = raw_input("y/n: ")
-                if add_answer1 == "y":
-                    item()
-                    break
-                elif add_answer1 == "n":
-                    print "no"
-                    break
-                else:
-                    print "Invalyd Option, insert y or n"
+            article_new()
+
+def article_new():
+    while True:
+        add_answer1 = raw_input("y/n: ")
+        if add_answer1 == "y":
+            print " "
+            answer_y_n()
+            break
+        elif add_answer1 == "n":
+            print " "
+            print "no"
+            break
+        else:
+            print "Invalyd Option, insert y or n"
 
 def menu():
     print " "
@@ -22,9 +27,7 @@ def menu():
     print "  1. Add an item: "
     print "  2. Sell Articles: "
     print "  3. Exit. "
-    item()
-
-def item():
+    print " "
     while True:
         answer = raw_input("Enter the number of your choice: ")
         if answer == "1":
