e30afa9fa0b5f70bd7810765e3e425294b5a1376
MarielyLopez <mariielylopez795@gmail.com>
2015-06-10 16:32:21 -0600

    Second rejected story made and checked.

--- application.py
+++ application.py
@@ -1,59 +1,83 @@
 #coding:utf8
 """Register Machine"""
-import string
-ARTICLES = {'Manzana':1.25, 'Pera':2.00, 'Sandia':10, 'Naranja':2.00,
+ARTICULOS = {'Manzana':1.25, 'Pera':2.00, 'Sandia':10, 'Naranja':2.00,
             'Mango':3.00,'Banano':1.00, 'Melon':5, 'Piña':5,}
-ENTER_ARTICLES = {}
+ARTICULOS_EN_INVENTARIO = {}
+CANTIDAD_DE_ARTICULOS = {}
 
 
 #while True:
-def answer_y_n():
+def ingresando_articulos():
+    """Pedira al ususario ingresar un articulo."""
+    respuesta_de_usuario = raw_input("Add an item: ")
+    respuesta_de_usuario = respuesta_de_usuario.lower()
+    return respuesta_de_usuario
 
+
+def ingresando_precio():
     while True:
-        """Pedira al ususario ingresar un articulo."""
-        add_answer = raw_input("Add an item: ")
-        add_answer = add_answer.lower()
-        while True:
-            try:
-                add_price = float(raw_input("Price: "))
-                article_new()
-                #_price es el resultado de  funcionENTER_ARTICLEStieneelproductoingresadoelususarioes "add_ARTICLES"
-                ENTER_ARTICLES[add_answer] = add_price
-                articlenew(add_answer)
-            except ValueError:
-
-                print "Insert only numbers and floats."
-                while True:
-                    break
-                    print ENTER_ARTICLES
-
-
-
-def articlenew(add_answer):
-    if add_answer == "done":
-        print "Sell Articles"
-    else:
-        print "Insert an item"
-        answer_y_n()
-
-#print "Do you want to insert another article?"M
-#article_new()
-
-def article_new():
-#verificara si la respuesta es no o si y ejecutara el programa sgun la condicion.
-    while True:
-        print "  Do you do insert a new article?"
-        add_answer1 = raw_input("y/n: ")
-        if add_answer1 == "y":
-            print " "
-            answer_y_n()
+        try:
+            definiendo_precio = float(raw_input("Ingresar precio: "))
+            return definiendo_precio
             break
-        elif add_answer1 == "n":
+        except:
+            print "No es un numero"
+
+def ingresando_cantidad():
+    while True:
+        try:
+            definiendo_precio = int(raw_input("Ingresar cantidad: "))
+            return definiendo_precio
+            break
+        except:
+            print "No es un numero"
+
+def funciones_agregar_item():
+    articulo = ingresando_articulos()
+    precio = ingresando_precio()
+    cantidad = ingresando_cantidad()
+    guardar_articulo(articulo, precio, cantidad)
+
+
+def guardar_articulo(articulo, precio, cantidad):
+    ARTICULOS_EN_INVENTARIO[articulo] = precio
+    CANTIDAD_DE_ARTICULOS[articulo] = cantidad
+    print ARTICULOS_EN_INVENTARIO
+    print ""
+    print CANTIDAD_DE_ARTICULOS
+    print " "
+    pregunta_si_o_no()
+
+
+def pregunta_si_o_no():
+    while True:
+        print "  Quiere ingresar otro articulo??"
+        respuesta_usuario = raw_input("si/no: ")
+        if respuesta_usuario == "si":
+            print ""
+            funciones_agregar_item()
+            break
+        elif respuesta_usuario == "no":
             print " "
             menu()
             break
         else:
-            print "Invalyd Option, insert y or n"
+            print "Opcion Invalida , inserte si o no"
+
+
+def vender_articulos():
+    while True:
+        print""
+        vender_articulos_respuesta = ingresando_articulos()
+        if vender_articulos_respuesta == "done":
+            print " Hola que hace"
+#            guardar_articulo()
+#            print "hola que hace2"
+            break
+        else:
+            print "ingrese otro articulo"
+
+
 
 def menu():
     """Main menu"""
@@ -63,23 +87,21 @@ def menu():
     print "     Menu"
     print " "
     print "  1. Add an item: "
-    print "  2. Sell ARTICLES: "
+    print "  2. Sell ARTICULOS: "
     print "  3. Exit. "
     print " "
-    valuemenu = True
-    while valuemenu ==True:
-        answer = raw_input("Enter the number of your choice: ")
-        if answer == "1":
-            answer_y_n()
+    while True:
+        respuesta = raw_input("Enter the number of your choice: ")
+        if respuesta == "1":
+            funciones_agregar_item()
             break #Esto es para que me corte el programa cuando la funcion es true"
-        elif answer == "2":
-            print "Sell ARTICLES"
+        elif respuesta == "2":
+            print "Sell ARTICULOS"
+            vender_articulos()
             break
-        elif answer == "3":
+        elif respuesta == "3":
             print "Exit"
-            valuemenu == False
             break
-
         else:
             print "Insert a number of 1 of 3"
 
